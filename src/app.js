f578db1bfffa911e76e58a2dedd5e5b6b6be604f
ottotogaev <otabeekt@gmail.com>
2022-05-19 20:27:08 +0500

    modified files

--- src/app.js
+++ src/app.js
@@ -4,6 +4,10 @@ const server = app.listen(5000, console.log('Proxy server is running on port 500
 const got = require('got')
 const cors = require('cors');
 
+// Tulind Functions
+const { sma_inc } = require('./indicators');
+
+
 app.use(cors());
 
 app.get('/', (_, res) => res.status(200).send('Proxy server works!'));
@@ -13,13 +17,14 @@ app.get('/:symbol/:interval', async (req, res) => {
         const url = 'https://api.binance.com/api/v3/klines?symbol=' + symbol + '&interval=' + interval
         const response = await got(url);
         const data = JSON.parse(response.body);
-        const klinesdata = data.map(d => ({
+        let klinesdata = data.map(d => ({
             time: d[0] / 1000,
             open: +d[1],
             high: +d[2],
             low: +d[3],
             close: +d[4],
         }));
+        klinesdata = await sma_inc(klinesdata);
         res.status(200).json(klinesdata);
     } catch (err) {
         res.status(500).send(err);
